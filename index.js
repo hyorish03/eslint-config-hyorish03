39d90df2425b3f071765e05409c471577a6d6a7a
hyorish03 <hyorish2001@gmail.com>
2024-09-17 03:10:58 +0900

    Add semi, quotes rules

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const react = require("eslint-plugin-react");
-const eslintPluginImport = require("eslint-plugin-import");
-
-module.exports = {
-  plugins: { react, import: eslintPluginImport },
-  rules: {
-    "no-unused-vars": ["warn"],
-    // import 순서 강제
-    "import/order": [
-      "error",
-      {
-        groups: [
-          ["builtin", "external"], // 내장 모듈과 외부 모듈을 먼저
-          ["internal"], // 내부 모듈
-          ["parent", "sibling", "index"], // 상위/형제/인덱스 모듈 순서로
-        ],
-        "newlines-between": "always", // 그룹 간 줄바꿈을 강제
-        alphabetize: { order: "asc", caseInsensitive: true }, // 알파벳 순으로 정렬
+export default [
+  {
+    languageOptions: {
+      globals: {
+        MyGlobal: true,
       },
-    ],
+      rules: {
+        quotes: [2, "single"],
+        semi: [2, "always"],
+        "import/order": [
+          "error",
+          {
+            groups: [
+              ["builtin", "external"], // 내장 모듈과 외부 모듈을 먼저
+              ["internal"], // 내부 모듈
+              ["parent", "sibling", "index"], // 상위/형제/인덱스 모듈 순서로
+            ],
+            "newlines-between": "always", // 그룹 간 줄바꿈을 강제
+            alphabetize: { order: "asc", caseInsensitive: true }, // 알파벳 순으로 정렬
+          },
+        ],
+      },
+    },
   },
-};
+];
